0186a389d7fe4b85d46f6be7db25b0e2b725d0df
masaibar <orebatemoko@gmail.com>
2016-05-08 17:44:32 +0900

    LocationUtilの追加

---
+++ app/src/main/java/com/masaibar/homewifi/utils/LocationUtil.java
@@ -0,0 +1,86 @@
+package com.masaibar.homewifi.utils;
+
+import android.content.Context;
+import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationManager;
+import android.provider.Settings;
+import android.support.annotation.Nullable;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+/**
+ * Created by masaibar on 2016/05/08.
+ */
+public class LocationUtil {
+
+    public static boolean isEnabledGPS(Context context) {
+        LocationManager locationManager =
+                (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+    }
+
+    public static void jumtToGPSSettings(Context context) {
+        Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(intent);
+    }
+
+    /**
+     * ジオコーディングを行う 住所=>緯度経度
+     */
+    @Nullable
+    public static LatLng getLatLngFromAddress(Context context, String addressStr) {
+        Geocoder geocoder = new Geocoder(context, Locale.getDefault());
+        try {
+            List<Address> addressList = geocoder.getFromLocationName(addressStr, 1);
+            if (addressList != null && addressList.size() > 0) {
+                Address address = addressList.get(0);
+                return new LatLng(address.getLatitude(), address.getLongitude());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * リバースジオコーディングを行う 緯度経度=>住所
+     * http://seesaawiki.jp/w/moonlight_aska/d/%B0%CC%C3%D6%BE%F0%CA%F3%A4%AB%A4%E9%BD%BB%BD%EA%A4%F2%BC%E8%C6%C0%A4%B9%A4%EB
+     */
+    public static String getAddressFromLatLng(Context context, LatLng latLng) {
+        Geocoder geocoder = new Geocoder(context, Locale.getDefault());
+        StringBuffer stringBuffer = new StringBuffer();
+        try {
+            List<Address> addressList = geocoder.getFromLocation(latLng.latitude, latLng.longitude, 1);
+            for (Address address : addressList) {
+                int index = address.getMaxAddressLineIndex();
+                for (int i = 0; i <= index; i++) {
+                    stringBuffer.append(address.getAddressLine(i));
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return stringBuffer.toString();
+    }
+
+    /**
+     * 二点のLatLng間の距離を返す
+     */
+    public static float getDistanceMeters(LatLng latLng1, LatLng latLng2) {
+        float[] results = new float[3];
+        Location.distanceBetween(
+                latLng1.latitude, latLng1.longitude,
+                latLng2.latitude, latLng2.longitude,
+                results);
+
+        return results[0];
+    }
+}
